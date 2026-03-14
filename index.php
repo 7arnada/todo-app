cb22a8779048533859653671cbe0db6a2c2f142c
7arnada <gyfhh@outlook.jp>
2026-03-14 09:07:13 +0900

    変数の基本勉強

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "TODOアプリ!!";
+$number = 10;
+
+$number2 = 11;
+
+$number3 = $number + $number2;
+$number4 = "5";
+
+echo $number."<br>".$number2;
+
+echo $number + $number2;
+
+echo $number4;
+
+echo "数字は".$number3."です。";
+
 ?>
